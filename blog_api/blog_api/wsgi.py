a02e1a35821e3dc2267a5687b55274d394568f1e
Yosuke Ikei <warau.uichi@gmail.com>
2023-08-22 23:50:46 +0900

    fix syntax error

--- blog_api/blog_api/wsgi.py
+++ blog_api/blog_api/wsgi.py
@@ -14,4 +14,4 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_api.settings')
 
-application = Cling(get_wsgi_application()
+application = Cling(get_wsgi_application())
